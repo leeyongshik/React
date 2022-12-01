188bd4a0adb5f8bcd9627bd8735fc3dc1fde973d
leeyongshik <jk1405@naver.com>
2022-12-01 10:00:31 +0900

    11/30

---
+++ day02/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Test01 from './component/Test01';
+
+function App() {
+  return (
+    <div>
+      <Test01/>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
